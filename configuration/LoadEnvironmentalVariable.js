49dccfe4d712ae86cd0d571f9e76e12c9106729a
amennajam009 <amennajam7@gmail.com>
2023-10-11 21:08:28 +0500

    add

---
+++ configuration/LoadEnvironmentalVariable.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+
+let MyEnviroment = {};
+
+if(process.env.NODE_ENV === 'development'){
+     MyEnviroment = dotenv.config({path:`${__dirname}/../env`});
+}
+
+console.log(MyEnviroment);
---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+const DataBaseConfig = require('./configuration/config');
+const LoadMyEnvironmentalVariable = require('./configuration/LoadEnvironmentalVariable')
