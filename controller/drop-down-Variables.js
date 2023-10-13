7e5be9b3f8fe561460355ef55c02a1e64c117159
amennajam009 <amennajam7@gmail.com>
2023-10-13 22:44:04 +0500

    add

--- controller/drop-down-Variables.js
+++ controller/drop-down-Variables.js
@@ -2,28 +2,80 @@
 // In this Api i'm only getting the variables 
 const SendDropDownVariables = async (req,res) =>{
     try {
-        const MyObject = {
-            object1:{
-                id:1,
-                variable:"TestingVar1"
-            },
-            object2:{
-                id:2,
-                variable:"TestingVar2"
-            },
-            object3:{
-                id:3,
-                variable:"TestingVar3"
+        const MyVariabels = {
+            data: {
+               variable1:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+               ] ,
+              variable2:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],
+              variable3:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],
+              variable4:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],
+              variable5:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],
+              variable6:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],
+              variable7:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],
+              variable8:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],
+              variable9:[
+                {id:1,variable:"option1"},
+                {id:2,variable:"option2"},
+                {id:3,variable:"option3"},
+                {id:4,variable:"option4"},
+                {id:5,variable:"option5"},
+              ],     
             }
         }
         res.json({
-            data:MyObject
+            data:MyVariabels
         })
     } catch (error) {
         res.json({
             message:error.message,
-            data:null,
-
+            data:null
         })
     }
 }
